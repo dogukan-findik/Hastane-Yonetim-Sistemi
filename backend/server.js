28ebd1d6e8a8f0b32ac540bc9213d9e4535e0284
dogukan_findik <dogu.findik@gmail.com>
2025-05-19 17:08:26 +0300

    Yerel değişiklikler

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Veritabanına bağlan
 connectDB();
